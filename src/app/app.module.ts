dfc66374ae50dc31f74aa3a7b64404ec4e86087c
Luiza Fernandes <luhqfernandes@gmail.com>
2021-04-13 10:20:41 -0300

    students component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
   ],
   imports: [
     BrowserModule,
