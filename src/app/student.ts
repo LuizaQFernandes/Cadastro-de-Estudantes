ca1e2b6c81b74cf21d8745d2d4e98ad70dffadbd
Luiza Fernandes <luhqfernandes@gmail.com>
2021-04-13 10:27:24 -0300

    interface student added

---
+++ src/app/student.ts
@@ -0,0 +1,7 @@
+export interface Student {
+    ra: number;
+    name: string;
+    age: number;
+    class: string;
+    parentName: string;
+}
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Student} from '../student'
 
 @Component({
   selector: 'app-students',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+
+  student: Student = {
+    ra: 1234,
+    name: 'Ana',
+    age: 12,
+    class: '3° Grade',
+    parentName: 'Maria',
+  }
 
   constructor() { }
 
