ab63288ad9d7c1c212376ec8f382adb7014278ce
Luiza Fernandes <luhqfernandes@gmail.com>
2021-05-12 15:43:17 -0300

    css changes - student added

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -5,5 +5,6 @@ export const STUDENTS: Student[] = [
   { ra: 3515111, name: 'João Vinícios Santos', age: 15 , class: '7° ano', parentName: 'Carlos Santos' },
   { ra: 154791, name: 'Jenifer Alves Silva', age: 12 , class: '4° ano', parentName: 'José Henrique Silva' },
   { ra: 1069231, name: 'Clara Almeida de Oliveira', age: 12 , class: '4° ano', parentName: 'Ana Maria de Oliveira' },
+  { ra: 10692871, name: 'José Henrique Silveira', age: 16 , class: '9° ano', parentName: 'Regina Souza Silveira' },
 
 ];
